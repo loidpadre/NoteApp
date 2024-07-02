952fb5077037ebd20a9a226fd42a744addec7e83
Loid Padre <145937391+loidpadre@users.noreply.github.com>
2024-07-02 19:50:18 +0000

    cors configuration

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const app = express();
 const User = require("./model/userSchema");
 const port = 3000;
-app.use(express.urlencoded({ extends: true }));
+app.use(express.urlencoded({ extends: false }));
 app.use(express.json());
 app.use(cors());
 
